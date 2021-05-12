932792d0b4df5eab91b3da7e4d1e648d423bc3a3
USER <user@USERnoMacBook-puro.local>
2021-05-12 21:22:23 +0900

    不要な行を削除

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,20 +3,8 @@ const { use } = require('../app');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 const eventsController = require('../controllers/eventsController');
-const multer = require('multer')
 const path = require('path');
 const sharp = require('sharp');
-
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './public/images/')
-    },
-    filename: function(req, file, cb) {
-        cb(null, req.session.user.id + req.session.user.name + ".jpg")
-    }
-})
-const upload = multer({ storage: storage })
-
 
 router.get('/login', usersController.login);
 router.get('/register', usersController.register);
